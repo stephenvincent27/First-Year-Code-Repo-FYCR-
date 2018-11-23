f7942868e52b6e0bbdc6feebc585b7d0ff5ee64e
stephenvincent27 <42001691+stephenvincent27@users.noreply.github.com>
2018-11-23 15:47:16 +0530

    Updated name and made changes to the code.

--- Read Matrix Using Recursion.c
+++ Read Matrix Using Recursion.c
@@ -1,18 +1,5 @@
 #include <stdio.h>
 
- void readMatrix(int *p, int row, int col)
- {
-   int i, j;
-   for(i=0;i<row;i++)
-   {
-     for(j=0;j<col;j++)
-     {
-       printf("\nEnter the [%d][%d] element of matrix: ", i, j);
-       scanf("%d", (p+i*col+j));
-     }
-   }
- }
-
  void readMatrixR(int *p, int row, int col)
  {
    static int i=0, j=0;
@@ -53,12 +40,6 @@ int main()
 
   int A[a][b];
   readMatrixR(A[0],a,b);
-
-  printf("\nEnter the number of rows and column in matrix 2: ");
-  scanf("%d %d", &c, &d);
-
-  int B[c][d];
-  readMatrixR(B[0],c,d);
 
   printMatrix(A[0],a,b);
 
